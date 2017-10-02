5400f60a5e969ea2ae99ca361fb65373e8eab3d2
Eyal Liebermann אייל ליברמן <eyal.liebermann@gmail.com>
2017-10-02 10:56:25 +0000

    hola mundo exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html: "hello world!"
+      render html: "hello, world!"
   end
+  
+  def hola 
+      render html: "¡hola, mundo!"
+  end
+  
+  def goodbye
+      render html: "goodbye, world"
 end
